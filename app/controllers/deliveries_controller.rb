4fea593d098645198c28e37021662964531b0752
Merrill Huang <127697576+merrillhuang@users.noreply.github.com>
2024-09-30 18:45:25 +0000

    delivery index page displays deliveries ordered by arrive date

--- app/controllers/deliveries_controller.rb
+++ app/controllers/deliveries_controller.rb
@@ -3,7 +3,7 @@ class DeliveriesController < ApplicationController
     # if current_user == nil
       # redirect_to("/users/sign_in") this is how to do it if not using 'before_action :authenticate_user!' in ApplicationController, but would have to do it for every route/page
     # else 
-      matching_deliveries = Delivery.all
+      matching_deliveries = Delivery.all.order(:supposed_to_arrive_on)
 
       @list_of_deliveries = matching_deliveries.order({ :created_at => :desc })
 
