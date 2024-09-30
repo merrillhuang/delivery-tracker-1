b0c4411f2b028bd284fc3ef96a83540d78375eef
Merrill Huang <127697576+merrillhuang@users.noreply.github.com>
2024-09-30 16:58:03 +0000

    run migration to add user_id foreign key to deliveries table

--- app/models/delivery.rb
+++ app/models/delivery.rb
@@ -8,6 +8,7 @@
 #  supposed_to_arrive_on :date
 #  created_at            :datetime         not null
 #  updated_at            :datetime         not null
+#  user_id               :integer
 #
 class Delivery < ApplicationRecord
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_09_30_164704) do
+ActiveRecord::Schema[7.1].define(version: 2024_09_30_165625) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -20,6 +20,7 @@ ActiveRecord::Schema[7.1].define(version: 2024_09_30_164704) do
     t.text "details"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "user_id"
   end
 
   create_table "users", force: :cascade do |t|
