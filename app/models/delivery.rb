007394db1b6b853c033b8f3cbb2d0a8697acf6ed
Merrill Huang <127697576+merrillhuang@users.noreply.github.com>
2024-09-30 17:00:56 +0000

    add arrived column to deliveries with generate migration

--- app/models/delivery.rb
+++ app/models/delivery.rb
@@ -3,6 +3,7 @@
 # Table name: deliveries
 #
 #  id                    :bigint           not null, primary key
+#  arrived               :boolean
 #  description           :text
 #  details               :text
 #  supposed_to_arrive_on :date
